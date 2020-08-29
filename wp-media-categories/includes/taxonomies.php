e3fcd8bc9f5a31d093913e3b64ec10a952cccbf6
Pea <pea@misfist.com>
2020-08-29 13:40:19 -0400

    Add taxonomy arg filter
    The args would be modified using the `register_taxonomy_args` filter hook, but providing one seems nicer. :)

--- wp-media-categories/includes/taxonomies.php
+++ wp-media-categories/includes/taxonomies.php
@@ -15,7 +15,30 @@ defined( 'ABSPATH' ) || exit;
  * @since 0.1.0
  */
 function wp_media_categories_register_media_taxonomy() {
-	register_taxonomy( 'media_category', array( 'attachment' ), array(
+	$labels = array(
+		'name'              => _x( 'Categories', 		'Taxonomy General Name', 'wp-media-categories' ),
+		'singular_name'     => _x( 'Category', 			'Taxonomy Singular Name', 'wp-media-categories' ),
+		'menu_name'			=> __( 'Categories',        'wp-media-categories' ),
+		'all_items'			=> __( 'All Categories',    'wp-media-categories' ),
+		'edit_item'			=> __( 'Edit Category',     'wp-media-categories' ),
+		'view_item'			=> __( 'View Category',     'wp-media-categories' ),
+		'update_item'		=> __( 'Update Category',   'wp-media-categories' ),
+		'add_new_item'		=> __( 'Add New Category',  'wp-media-categories' ),
+		'new_item_name'		=> __( 'New Category Name', 'wp-media-categories' ),
+		'parent_item'		=> __( 'Parent Category',   'wp-media-categories' ),
+		'parent_item_colon'	=> __( 'Parent Category:',  'wp-media-categories' ),
+		'search_items'		=> __( 'Search Categories', 'wp-media-categories' )
+	);
+
+	$rewrite = array(
+		'with_front'   => false,
+		'heirarchical' => true,
+		'slug'         => 'media-category'
+	);
+
+	$args = array(
+		'labels'                => $labels,
+		'rewrite' 				=> $rewrite,
 		'hierarchical'			=> true,
 		'show_ui'				=> true,
 		'show_admin_column'		=> true,
@@ -25,24 +48,7 @@ function wp_media_categories_register_media_taxonomy() {
 		'update_count_callback'	=> 'wp_media_categories_update_count_callback',
 		'show_in_rest'          => true,
 		'rest_base'             => 'media-categories',
-		'labels'                => array(
-			'name'				=> __( 'Categories',        'wp-media-categories' ),
-			'singular_name'		=> __( 'Category',          'wp-media-categories' ),
-			'menu_name'			=> __( 'Categories',        'wp-media-categories' ),
-			'all_items'			=> __( 'All Categories',    'wp-media-categories' ),
-			'edit_item'			=> __( 'Edit Category',     'wp-media-categories' ),
-			'view_item'			=> __( 'View Category',     'wp-media-categories' ),
-			'update_item'		=> __( 'Update Category',   'wp-media-categories' ),
-			'add_new_item'		=> __( 'Add New Category',  'wp-media-categories' ),
-			'new_item_name'		=> __( 'New Category Name', 'wp-media-categories' ),
-			'parent_item'		=> __( 'Parent Category',   'wp-media-categories' ),
-			'parent_item_colon'	=> __( 'Parent Category:',  'wp-media-categories' ),
-			'search_items'		=> __( 'Search Categories', 'wp-media-categories' )
-		),
-		'rewrite' => array(
-			'with_front'   => false,
-			'heirarchical' => true,
-			'slug'         => 'media-category'
-		)
-	) );
+	);
+
+	register_taxonomy( 'media_category', array( 'attachment' ), apply_filters( 'wp_media_categories_taxonomy_args', $args ) );
 }
